a378b8a4fb7001a57fc951a0dcd04061905914d6
vitorgonaraujo <agente51411@gmail.com>
2025-07-08 19:43:00 -0300

    chore: updates

--- bookscraper/pipelines.py
+++ bookscraper/pipelines.py
@@ -10,6 +10,7 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+
         adapter = ItemAdapter(item)
         field_names = adapter.field_names()
 
